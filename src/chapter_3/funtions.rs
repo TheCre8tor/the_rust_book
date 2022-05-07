3dca9efb115bd8ef30fc9a20972d19100923c23d
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-07 11:38:05 +0100

    Learnt about how functions works

--- src/chapter_3/funtions.rs
+++ src/chapter_3/funtions.rs
@@ -1,1 +1,17 @@
-pub fn run() {}
+// TODO: Come back for this, later
+
+pub fn run() {
+    println!("Hello, world!");
+
+    another_function(-126);
+}
+
+fn another_function(x: i32) {
+   println!("Another function, with parameter of: {}", x);
+}
+
+/* Statements & Expression */
+
+/* Statement are instructions that perform 
+   some action and do not return a value */
+   
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ mod chapter_2;
 mod chapter_3;
 
 fn main() {
-    chapter_2::guess_game::run();
-    chapter_3::variables::run();
-    chapter_3::data_types::run();
+    // chapter_2::guess_game::run();
+    // chapter_3::variables::run();
+    // chapter_3::data_types::run();
     chapter_3::funtions::run();
 }
