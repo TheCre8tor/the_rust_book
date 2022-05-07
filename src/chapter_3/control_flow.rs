93c851a2bdbd26646277579eb7cfc41a646aaf95
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-07 11:52:16 +0100

    Handing Multiple Conditions with else if

--- src/chapter_3/control_flow.rs
+++ src/chapter_3/control_flow.rs
@@ -1,7 +1,7 @@
 pub fn control() {
     // if Expressions
 
-    let number: u8 = 13;
+    let number: u8 = 14;
 
     if number < 5 {
         println!("condition was true");
@@ -9,5 +9,15 @@ pub fn control() {
         println!("condition is false");
     }
 
-    
+    // Handing Multiple Conditions with else if
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
