71bef296f601451edb3960ba1851c58d2b32b654
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-08 07:34:46 +0100

    Learn't The Stack

--- src/chapter_4/intro.rs
+++ src/chapter_4/intro.rs
@@ -1,3 +1,22 @@
 pub fn owner() {
     println!("This is ownership");
+
+    /* SECTION  1. What is ownership:
+      Ownership is a set of rules that governs how a Rust program manages memory. 
+
+      ? The Stack ->
+        The stack stores values in the order it gets them and removes the values 
+        in the opposite order. This is referred to as last in, first out.
+
+        Adding data is called pushing onto the stack, and removing data is called 
+        popping off the stack. 
+        
+        All data stored on the stack must have a known, fixed size. Data with an 
+        unknown size at compile time or a size that might change must be stored 
+        on the heap instead.
+
+      
+      
+      */
+
 }
