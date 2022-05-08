92d22a32a71c1c3399214d69e0fa42bf3803158e
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-08 13:45:19 +0100

    I'm really enjoying The String Slices Tuts

--- src/chapter_4/the_slice_type.rs
+++ src/chapter_4/the_slice_type.rs
@@ -1,20 +1,30 @@
 pub fn run() {
     let word = String::from("How are you!");
+
     let result = first_world(&word);
 
-    let str_slice = &word[0..result];
+    println!("First word is: {}", result);
 
-    println!("First word is: {}", str_slice);
+    // SECTION  2. String Slices
+    /* A string slice is a reference to part of a String, 
+       and it looks like this: */
+
+    let string = String::from("Hello world! ");
+
+    let hello = &string[0..5];
+    let world = &string[6..12];
+
+    println!("First Slice: {}, Second Slice:{}", hello, world);
 }
 
-fn first_world(stra: &String) -> usize {
-    let bytes = stra.as_bytes();
+fn first_world(stra: &String) -> &str {
+    let word = stra.as_bytes();
 
-    for (idx, &item) in bytes.iter().enumerate() {
+    for (idx, &item) in word.iter().enumerate() {
         if item == b' ' {
-            return idx;
+            return &stra[0..idx];
         }
     }
 
-    stra.len()
+    &stra[..]
 }
